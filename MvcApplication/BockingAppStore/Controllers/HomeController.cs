72bbf00bed210c78f0bac4a132cf085fdfc3f873
Bevz Vyacheslav <paracord9@ukr.net>
2020-08-08 15:28:41 +0300

    Create controller for Book

--- MvcApplication/BockingAppStore/Controllers/HomeController.cs
+++ MvcApplication/BockingAppStore/Controllers/HomeController.cs
@@ -18,14 +18,16 @@ namespace BockingAppStore.Controllers
         }
 
         [HttpGet]
-        public ActionResult Buy( int id){
+        public ActionResult Buy(int id)
+        {
             ViewBag.BookId = id;
             return View();
         }
 
         //данный метод будет обрабатываться пост
         [HttpPost]
-        public string Buy (Purchase purchase){
+        public string Buy(Purchase purchase)
+        {
 
             purchase.Date = DateTime.Now;
             //сохраняем в бд
@@ -33,6 +35,40 @@ namespace BockingAppStore.Controllers
             //сохраняем в бд все изменения
             db.SaveChanges();
             return "Спасибо, " + purchase.Person + ", за покупку!";
+        }
+        //http://localhost:60958/home/Square?a=10&h=30
+        //public string Square(int a, int h)
+        //{
+        //    double s = a * h / 2;
+        //    return "<h2> Площадь треугольника с основанием " + a + " и высотой " + h + " равна " + s + "</h2>";
+        //}
+
+        //http://localhost:60958/home/Square?a=10&h=30
+        public string Square()
+        {
+            int a = Int32.Parse(Request.Params["a"]);
+            int h = Int32.Parse(Request.Params["h"]);
+            double s = a * h / 2;
+            return "<h2> Площадь треугольника с основанием " + a + " и высотой " + h + " равна " + s + "</h2>";
+        }
+
+        [HttpGet]
+        public ActionResult GetBook()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string PostBook()
+        {
+            string title = Request.Form["title"];
+            string author = Request.Form["author"];
+            return title +" "+ author;
+        }
+
+        public string GetId(int id)
+        {
+            return id.ToString();
         }
         public ActionResult About()
         {
